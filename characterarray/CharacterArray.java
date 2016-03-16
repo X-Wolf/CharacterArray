f01b4e054edfa9ca62b5fe48c3c7e80d3cfc5c34
X-Wolf <pascualxuaco@gmail.com>
2016-03-15 22:57:59 -0400

    Final

--- characterarray/CharacterArray.java
+++ characterarray/CharacterArray.java
@@ -6,7 +6,9 @@
 package characterarray;
 
 /**
- *
+ *Unit 3 Problem 6. The purpose of this program is to take an String provided
+ * and convert it into an array. Then turn lower case letters to upper case
+ * and vice versa and turn digits into asterisks.
  * @author Xuaco
  */
 public class CharacterArray {
@@ -20,13 +22,13 @@ public class CharacterArray {
 
             System.out.print((Character)chars[i] + " ");
             if(Character.isUpperCase(chars[i])){
-                System.out.print(Character.toLowerCase(chars[i]) + " Letter \n");
+                System.out.print("is a letter " + Character.toLowerCase(chars[i]) + "\n");
                 chars2[i] = (Character.toLowerCase(chars[i]));
             } else if(Character.isDigit(chars[i])){
-                System.out.print("* Digit" +"\n");
+                System.out.print("is a Digit  *" +"\n");
                 chars2[i] = '*';
             } else {
-                System.out.print(Character.toUpperCase(chars[i]) + " Letter \n");
+                System.out.print("is a letter " + Character.toUpperCase(chars[i]) + "\n");
                 chars2[i] = (Character.toUpperCase(chars[i]));
             }
 
