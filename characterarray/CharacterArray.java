6649283acfe7b53a9bb371e8011ae3a258e70529
X-Wolf <pascualxuaco@gmail.com>
2016-03-15 22:29:13 -0400

    Adjust

--- characterarray/CharacterArray.java
+++ characterarray/CharacterArray.java
@@ -13,7 +13,7 @@ public class CharacterArray {
     
     public static void main(String[] args) {
         char[] chars = "6901 Sudley road Manassas VA".toCharArray();
-        System.out.println("The original array is " + String.valueOf(char[]));
+        System.out.println("The original array is " + String.valueOf(chars));
     }
     
 }
